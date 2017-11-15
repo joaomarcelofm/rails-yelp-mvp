ccb484469aff90bc737e72ffbfd03ae7649a3974
Joao Martins <joaomarcelofm@gmail.com>
2017-11-15 11:54:43 +0100

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  get 'restaurants/index'
+
+  get 'restaurants/show'
+
+  get 'restaurants/new'
+
+  get 'restaurants/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
