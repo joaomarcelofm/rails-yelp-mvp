8c89bacf5b6bad0723d623ca2210b97bce5e907f
Joao Martins <joaomarcelofm@gmail.com>
2017-11-15 14:46:25 +0100

    done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,31 @@
 class ReviewsController < ApplicationController
+
+  before_action :set_restaurant, only: [:create, :new]
+
   def index
+    @reviews = Review.all
   end
 
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to @restaurant
+    else
+      render 'new'
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
+  # get 'reviews/index'
 
-  get 'reviews/new'
+  # get 'reviews/new'
 
-  get 'reviews/create'
+  # get 'reviews/create'
 
-  get 'restaurants/index'
+  # get 'restaurants/index'
 
-  get 'restaurants/show'
+  # get 'restaurants/show'
 
-  get 'restaurants/new'
+  # get 'restaurants/new'
 
-  get 'restaurants/edit'
+  # get 'restaurants/edit'
+
+  root to: 'restaurants#index'
+
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
